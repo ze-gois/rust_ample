a87dc6f2a3a6b3a66129627c09013039ed8bdbb4
José Gois <ze.gois.00@gmail.com>
2025-08-31 16:47:01 -0300

    slime

--- src/library.rs
+++ src/library.rs
@@ -1,13 +1,5 @@
 #![no_std]
-#![no_main]
 #![allow(incomplete_features)]
 #![allow(unused_assignments)]
 #![feature(generic_const_exprs)]
 #![feature(generic_const_items)]
-
-pub use userspace::info;
-pub use userspace::target;
-
-pub fn x() {
-    info!("number {}", 9);
-}
