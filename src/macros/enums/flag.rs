cd8f2751409fc6d92da4e5c3d203f92590189c02
José Gois <ze.gois.00@gmail.com>
2025-09-20 02:21:06 -0300

    initing main

--- src/macros/enums/flag.rs
+++ src/macros/enums/flag.rs
@@ -10,8 +10,8 @@
 /// # Arguments
 ///
 /// * `$enum_identifier` - The name of the error enum (usually Error)
-/// * `$variant` - The variant name in the crate::result::Error enum (e.g., Open, Read, Write)
-/// * `$label` - String slice with the syscall name
+/// * `$enum_discriminant_type` - The variant name in the crate::result::Error enum (e.g., Open, Read, Write)
+/// * `$enum_label` - String slice with the syscall name
 /// * A list of error variants with their descriptions, discriminant values and Linux standard constant names
 ///   [VariantName, discriminant_value, "description", "LINUX_CONSTANT"]
 ///
@@ -19,16 +19,16 @@
 ///
 /// ```
 /// $enum_identifier:ident,
-/// $variant:ty,
-/// $label:expr,
+/// $enum_discriminant_type:ty,
+/// $enum_label:expr,
 /// [
 ///     $(
 ///         [
-///             $discriminant:expr;
-///             $identifier:ident;
-///             $const_identifier:ident;
-///             $acronym:expr;
-///             $description:expr
+///             $variant_discriminant:expr;
+///             $variant_identifier:ident;
+///             $variant_constant_identifier:ident;
+///             $variant_acronym:expr;
+///             $variant_description:expr
 ///         ]
 ///     ),* $(,)?
 /// ]
@@ -37,69 +37,140 @@
 #[rustfmt::skip]
 macro_rules! enum_flag {
     (
-        $enum_identifier:ident,
-        $variant:ty,
-        $label:expr,
-        [
+        $enum_discriminant_type:ty;
+        $enum_label:expr;
+        $(#[$($enum_meta:meta),*])*
+        $enum_vis:vis enum $enum_identifier:ident {
             $(
                 [
-                    $discriminant:expr;
-                    $identifier:ident;
-                    $const_identifier:ident;
-                    $acronym:expr;
-                    $description:expr
+                    $variant_discriminant:expr;
+                    $variant_identifier:ident;
+                    $variant_constant_identifier:ident;
+                    $variant_acronym:expr;
+                    $variant_description:expr
                 ]
             ),* $(,)?
-        ]
+        }
     ) => {
         // Define Linux standard error constants in an discriminant module with standard names
         pub mod constants {
             $(
-                pub const $const_identifier: $variant = $discriminant;
+                pub const $variant_constant_identifier: $enum_discriminant_type = $variant_discriminant;
             )*
         }
 
+
+        // pub enum $enum_identifier {
+        //     $($variant_identifier = $variant_discriminant,)*
+        //     TODO,
+        // }
+
+        // $crate::r#enum!(
+        //     $enum_discriminant_type;
+        //     $(#[$($enum_meta),*])*
+        //     #[repr(C)]
+        //     #[derive(Copy, Clone, Eq, PartialEq)]
+        //     $enum_vis enum $enum_identifier {
+        //         $(
+        //             $variant_identifier = $variant_discriminant,
+        //         )*
+        //         TODO = <$enum_discriminant_type>::MAX,
+        //     }
+        // );
+
+
+        $(#[$($enum_meta),*])*
         #[repr(C)]
         #[derive(Copy, Clone, Eq, PartialEq)]
-        pub enum $enum_identifier {
-            $($identifier = $discriminant,)*
+        $enum_vis enum $enum_identifier {
+            $(
+                $variant_identifier,
+            )*
             TODO,
         }
 
         impl $enum_identifier {
-            pub fn from(discriminant: $variant) -> Self {
+            pub fn discriminant(&self) -> $enum_discriminant_type {
+                match self {
+                    $(
+                        $enum_identifier::$variant_identifier => $variant_discriminant,
+                    )*
+                    $enum_identifier::TODO => <$enum_discriminant_type>::MAX,
+                }
+            }
+        }
+
+        impl $crate::traits::Bytes<crate::Origin, crate::Origin> for $enum_identifier {
+            const BYTES_SIZE : usize = <$enum_discriminant_type as $crate::traits::Bytes<crate::Origin, crate::Origin>>::BYTES_SIZE;
+            const BYTES_ALIGN : usize = <$enum_discriminant_type as $crate::traits::Bytes<crate::Origin, crate::Origin>>::BYTES_ALIGN;
+
+            fn to_bytes(&self, endianness: bool) -> [u8;<Self as $crate::traits::Bytes<crate::Origin, crate::Origin>>::BYTES_SIZE] {
+                let mut bytes = [0u8;<Self as $crate::traits::Bytes<crate::Origin, crate::Origin>>::BYTES_SIZE];
+                let discriminant = self.discriminant();
+                match self {
+                    $(
+                        Self::$variant_identifier => {
+
+                            let mut o = 0;
+                            bytes[o..(o+<$enum_discriminant_type as $crate::traits::Bytes<crate::Origin, crate::Origin>>::BYTES_SIZE)].copy_from_slice(
+                                &<$enum_discriminant_type as $crate::traits::Bytes<crate::Origin, crate::Origin>>::to_bytes(&discriminant,endianness)
+                            );
+                            bytes
+                        },
+                    )*
+                    Self::TODO => <$enum_discriminant_type as $crate::traits::Bytes<crate::Origin, crate::Origin>>::to_bytes(&discriminant, endianness)
+                }
+            }
+
+            fn from_bytes(bytes: [u8;<Self as $crate::traits::Bytes<crate::Origin, crate::Origin>>::BYTES_SIZE], endianness: bool) -> Self {
+                let mut o = 0;
+                let mut discriminant_bytes = [0u8; <$enum_discriminant_type as $crate::traits::Bytes<crate::Origin, crate::Origin>>::BYTES_SIZE];
+                discriminant_bytes.copy_from_slice(&bytes[o..(o+<$enum_discriminant_type as $crate::traits::Bytes<crate::Origin, crate::Origin>>::BYTES_SIZE)]);
+                let discriminant = <$enum_discriminant_type as $crate::traits::Bytes<crate::Origin, crate::Origin>>::from_bytes(discriminant_bytes, endianness);
                 match discriminant {
-                    $($discriminant => Self::$identifier,)*
+                    $(
+                        $variant_discriminant => {
+                            Self::$variant_identifier
+                        },
+                    )*
+                    _ => Self::TODO
+                }
+
+            }
+        }
+
+        impl $enum_identifier {
+            pub fn from(discriminant: $enum_discriminant_type) -> Self {
+                match discriminant {
+                    $($variant_discriminant => Self::$variant_identifier,)*
                     _ => Self::TODO,
                 }
             }
 
-            pub fn to(&self) -> $variant {
+            pub fn to(&self) -> $enum_discriminant_type {
                 match *self {
-                    $(Self::$identifier => $discriminant,)*
-                    _ => <$variant>::MAX
+                    $(Self::$variant_identifier => $variant_discriminant,)*
+                    _ => <$enum_discriminant_type>::MAX
                 }
             }
 
             pub fn str(&self) -> &str {
                 match self {
-                    $(Self::$identifier => $description,)*
+                    $(Self::$variant_identifier => $variant_description,)*
                     _ => "TODO"
                 }
             }
 
             pub fn acronym(&self) -> &str {
                 match *self {
-                    $(Self::$identifier => $acronym,)*
+                    $(Self::$variant_identifier => $variant_acronym,)*
                     _ => "Unknown error",
                 }
             }
         }
 
-
-
         impl core::ops::BitOr for $enum_identifier {
-            type Output = usize;
+            type Output = $enum_discriminant_type;
             fn bitor(self, rhs: Self) -> Self::Output {
                 self.to() | rhs.to()
             }
@@ -134,4 +205,5 @@ macro_rules! enum_flag {
         }
     };
 }
+
 pub use enum_flag;
