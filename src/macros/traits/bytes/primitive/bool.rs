505c01b6778157dee7be3f6a18c787aac0448790
Zé Gois <2962558+ze-gois@users.noreply.github.com>
2026-09-21 15:25:13 -0300

    refactor: name representation size explicitly

--- src/macros/traits/bytes/primitive/bool.rs
+++ src/macros/traits/bytes/primitive/bool.rs
@@ -2,15 +2,15 @@
 macro_rules! trait_implement_primitive_bool_bytes {
     () => {
         impl $crate::traits::Bytes<crate::Origin, crate::Origin> for bool {
-            const BYTES_SIZE: usize =
-                <u8 as $crate::traits::Bytes<crate::Origin, crate::Origin>>::BYTES_SIZE;
+            const REPRESENTATION_SIZE: usize =
+                <u8 as $crate::traits::Bytes<crate::Origin, crate::Origin>>::REPRESENTATION_SIZE;
             const BYTES_ALIGN: usize =
                 <u8 as $crate::traits::Bytes<crate::Origin, crate::Origin>>::BYTES_ALIGN;
 
             fn to_bytes(
                 &self,
                 _endianness: bool,
-            ) -> [u8; <Self as $crate::traits::Bytes<crate::Origin, crate::Origin>>::BYTES_SIZE]
+            ) -> [u8; <Self as $crate::traits::Bytes<crate::Origin, crate::Origin>>::REPRESENTATION_SIZE]
             {
                 <u8 as $crate::traits::Bytes<crate::Origin, crate::Origin>>::to_le_bytes(
                     &(*self as u8),
@@ -19,7 +19,7 @@ macro_rules! trait_implement_primitive_bool_bytes {
 
             fn from_bytes(
                 bytes: [u8;
-                    <Self as $crate::traits::Bytes<crate::Origin, crate::Origin>>::BYTES_SIZE],
+                    <Self as $crate::traits::Bytes<crate::Origin, crate::Origin>>::REPRESENTATION_SIZE],
                 _endianness: bool,
             ) -> Self {
                 bytes[0] != 0
