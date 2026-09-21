8135315bed08ddf92372cef2480295c3c77d5cf0
Zé Gois <2962558+ze-gois@users.noreply.github.com>
2026-09-21 15:24:42 -0300

    refactor: name representation size explicitly

--- src/macros/traits/bytes/slice.rs
+++ src/macros/traits/bytes/slice.rs
@@ -14,19 +14,19 @@ macro_rules! trait_implement_bytes_slice {
                 $($where_alias : $($where_boundary)::* $(<$($($where_boundary_generics)::*),*>)?),*
             )?
             {
-                const BYTES_SIZE: usize =
-                    N * <$($type)::* as $crate::traits::Bytes<crate::Origin, crate::Origin>>::BYTES_SIZE;
+                const REPRESENTATION_SIZE: usize =
+                    N * <$($type)::* as $crate::traits::Bytes<crate::Origin, crate::Origin>>::REPRESENTATION_SIZE;
                     const BYTES_ALIGN: usize = <$($type)::* as $crate::traits::Bytes<crate::Origin, crate::Origin>>::BYTES_ALIGN;
 
                 fn to_bytes(
                     &self,
                     endianness: bool,
-                ) -> [u8; <Self as $crate::traits::Bytes<crate::Origin, crate::Origin>>::BYTES_SIZE]
+                ) -> [u8; <Self as $crate::traits::Bytes<crate::Origin, crate::Origin>>::REPRESENTATION_SIZE]
                 where
-                    [(); <Self as $crate::traits::Bytes<crate::Origin, crate::Origin>>::BYTES_SIZE]:,
+                    [(); <Self as $crate::traits::Bytes<crate::Origin, crate::Origin>>::REPRESENTATION_SIZE]:,
                 {
-                    let mut bytes = [0u8; <Self as $crate::traits::Bytes<crate::Origin, crate::Origin>>::BYTES_SIZE];
-                    let item_size = <$($type)::* as $crate::traits::Bytes<crate::Origin, crate::Origin>>::BYTES_SIZE;
+                    let mut bytes = [0u8; <Self as $crate::traits::Bytes<crate::Origin, crate::Origin>>::REPRESENTATION_SIZE];
+                    let item_size = <$($type)::* as $crate::traits::Bytes<crate::Origin, crate::Origin>>::REPRESENTATION_SIZE;
 
                     for (i, item) in self.iter().enumerate() {
                         let item_bytes = <$($type)::* as $crate::traits::Bytes<crate::Origin, crate::Origin>>::to_bytes(
@@ -39,15 +39,15 @@ macro_rules! trait_implement_bytes_slice {
                     bytes
                 }
 
-                fn from_bytes(bytes: [u8; <Self as $crate::traits::Bytes<crate::Origin, crate::Origin>>::BYTES_SIZE], endianness: bool) -> Self
+                fn from_bytes(bytes: [u8; <Self as $crate::traits::Bytes<crate::Origin, crate::Origin>>::REPRESENTATION_SIZE], endianness: bool) -> Self
                 where
-                    [(); <Self as $crate::traits::Bytes<crate::Origin, crate::Origin>>::BYTES_SIZE]:,
+                    [(); <Self as $crate::traits::Bytes<crate::Origin, crate::Origin>>::REPRESENTATION_SIZE]:,
                 {
-                    const NN: usize = <$($type)::* as $crate::traits::Bytes<crate::Origin, crate::Origin>>::BYTES_SIZE;
+                    const NN: usize = <$($type)::* as $crate::traits::Bytes<crate::Origin, crate::Origin>>::REPRESENTATION_SIZE;
                     let defaulta = <$($type)::* as $crate::traits::Bytes<crate::Origin, crate::Origin>>::from_bytes([0u8; NN], endianness);
                     let mut arr: [$($type)::*; N] = [defaulta; N];
-                    for (i, chunk) in bytes.chunks_exact(<$($type)::* as $crate::traits::Bytes<crate::Origin, crate::Origin>>::BYTES_SIZE).enumerate() {
-                        let mut buf = [0u8; <$($type)::* as $crate::traits::Bytes<crate::Origin, crate::Origin>>::BYTES_SIZE];
+                    for (i, chunk) in bytes.chunks_exact(<$($type)::* as $crate::traits::Bytes<crate::Origin, crate::Origin>>::REPRESENTATION_SIZE).enumerate() {
+                        let mut buf = [0u8; <$($type)::* as $crate::traits::Bytes<crate::Origin, crate::Origin>>::REPRESENTATION_SIZE];
                         buf.copy_from_slice(chunk);
                         arr[i] = <$($type)::* as $crate::traits::Bytes<crate::Origin, crate::Origin>>::from_bytes(buf, endianness);
                     }
@@ -57,17 +57,17 @@ macro_rules! trait_implement_bytes_slice {
 
 
                 fn from_bytes_pointer(bytes_pointer: *const u8, endianness: bool) -> Self {
-                    const NN: usize = <$($type)::* as $crate::traits::Bytes<crate::Origin, crate::Origin>>::BYTES_SIZE;
+                    const NN: usize = <$($type)::* as $crate::traits::Bytes<crate::Origin, crate::Origin>>::REPRESENTATION_SIZE;
                     let defaulta = <$($type)::* as $crate::traits::Bytes<crate::Origin, crate::Origin>>::from_bytes([0u8; NN], endianness);
                     let mut arr: [$($type)::*; N] = [defaulta; N];
                     for i in 0..N {
-                        let mut buf = [0u8; <$($type)::* as $crate::traits::Bytes<crate::Origin, crate::Origin>>::BYTES_SIZE];
+                        let mut buf = [0u8; <$($type)::* as $crate::traits::Bytes<crate::Origin, crate::Origin>>::REPRESENTATION_SIZE];
                         unsafe { core::ptr::copy_nonoverlapping(bytes_pointer.add(i*NN), buf.as_mut_ptr(), NN) };
                         // buf.copy_from_slice(unsafe { std::slice::from_raw_parts(bytes_pointer.add(i * NN), NN) });
                         arr[i] = <$($type)::* as $crate::traits::Bytes<crate::Origin, crate::Origin>>::from_bytes(buf, endianness);
                     }
-                    // for (i, chunk) in bytes.chunks_exact(<$($type)::* as $crate::traits::Bytes<crate::Origin, crate::Origin>>::BYTES_SIZE).enumerate() {
-                    //     let mut buf = [0u8; <$($type)::* as $crate::traits::Bytes<crate::Origin, crate::Origin>>::BYTES_SIZE];
+                    // for (i, chunk) in bytes.chunks_exact(<$($type)::* as $crate::traits::Bytes<crate::Origin, crate::Origin>>::REPRESENTATION_SIZE).enumerate() {
+                    //     let mut buf = [0u8; <$($type)::* as $crate::traits::Bytes<crate::Origin, crate::Origin>>::REPRESENTATION_SIZE];
                     //     buf.copy_from_slice(chunk);
                     //     arr[i] = <$($type)::* as $crate::traits::Bytes<crate::Origin, crate::Origin>>::from_bytes(buf, endianness);
                     // }
@@ -75,12 +75,12 @@ macro_rules! trait_implement_bytes_slice {
 
                     // let mut _o = 0;
                     // $(
-                    //     let mut field_bytes = [0u8; <$field_type as $crate::traits::Bytes<crate::Origin, crate::Origin>>::BYTES_SIZE];
+                    //     let mut field_bytes = [0u8; <$field_type as $crate::traits::Bytes<crate::Origin, crate::Origin>>::REPRESENTATION_SIZE];
                     //     unsafe {
-                    //         core::ptr::copy_nonoverlapping(bytes_pointer.add(_o), field_bytes.as_mut_ptr(), <$field_type as $crate::traits::Bytes<crate::Origin, crate::Origin>>::BYTES_SIZE);
+                    //         core::ptr::copy_nonoverlapping(bytes_pointer.add(_o), field_bytes.as_mut_ptr(), <$field_type as $crate::traits::Bytes<crate::Origin, crate::Origin>>::REPRESENTATION_SIZE);
                     //     }
                     //     let $field_identifier = <$field_type as $crate::traits::Bytes<crate::Origin, crate::Origin>>::from_bytes(field_bytes,endianness);
-                    //     _o = _o + <$field_type as $crate::traits::Bytes<crate::Origin, crate::Origin>>::BYTES_SIZE;
+                    //     _o = _o + <$field_type as $crate::traits::Bytes<crate::Origin, crate::Origin>>::REPRESENTATION_SIZE;
                     // )*
                     // Self {
                     //     $($field_identifier,)*
