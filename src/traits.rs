b05d82f1c2a781522821a003ccb1c8bb738b014b
Zé Gois <2962558+ze-gois@users.noreply.github.com>
2026-09-21 15:41:21 -0300

    refactor: make Allocating the allocation abstraction

--- src/traits.rs
+++ src/traits.rs
@@ -1,11 +1,8 @@
-pub mod allocatable;
 pub mod allocating;
 pub mod bytes;
 pub mod enums;
 pub mod primitive;
 
-pub use allocatable::Allocatable;
-pub use allocatable::AllocatableResult;
 pub use allocating::Allocating;
 pub use bytes::Bytes;
 pub use bytes::BytesDefault;
