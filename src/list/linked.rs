2be3ce1e9d6938be783f50af609841949f3d590f
Zé Gois <2962558+ze-gois@users.noreply.github.com>
2026-09-21 15:40:39 -0300

    refactor: decouple linked structures from representation

--- src/list/linked.rs
+++ src/list/linked.rs
@@ -1,196 +1,145 @@
 use crate::node::LinkedNode;
-use crate::traits::Allocatable;
-use crate::traits::Bytes;
-use crate::traits::allocatable::AllocatableResult;
-// use crate::traits::allocatable::HeapAllocatable;
+use crate::traits::Allocating;
 use core::marker::PhantomData;
 
-/// A singly linked list implementation that can work with different allocation strategies
 #[derive(Debug)]
-pub struct LinkedList<BytesOrigin, BytesDestination, AllocatorOrigin, B, A>
+pub struct LinkedList<T, A>
 where
-    B: Bytes<BytesOrigin, BytesDestination>,
-    A: Allocatable<AllocatorOrigin>,
+    A: Allocating,
 {
-    meta: Option<*mut B>,
-    former: Option<*mut LinkedNode<BytesOrigin, BytesDestination, AllocatorOrigin, B>>,
-    latter: Option<*mut LinkedNode<BytesOrigin, BytesDestination, AllocatorOrigin, B>>,
-    numerosity: usize,
-    _phantom_o: PhantomData<BytesOrigin>,
-    _phantom_d: PhantomData<BytesDestination>,
-    _phantom_p: PhantomData<AllocatorOrigin>,
-    _phantom_a: PhantomData<A>,
+    head: Option<*mut LinkedNode<T>>,
+    tail: Option<*mut LinkedNode<T>>,
+    length: usize,
+    allocator: PhantomData<A>,
 }
 
-impl<BytesOrigin, BytesDestination, AllocatorOrigin, B, A>
-    LinkedList<BytesOrigin, BytesDestination, AllocatorOrigin, B, A>
+impl<T, A> LinkedList<T, A>
 where
-    B: Bytes<BytesOrigin, BytesDestination>,
-    A: Allocatable<AllocatorOrigin>,
+    A: Allocating,
 {
-    /// Create a new empty linked list
-    pub fn new() -> Self {
+    pub const fn new() -> Self {
         Self {
-            meta: None,
-            former: None,
-            latter: None,
-            numerosity: 0,
-            _phantom_o: PhantomData,
-            _phantom_d: PhantomData,
-            _phantom_p: PhantomData,
-            _phantom_a: PhantomData,
+            head: None,
+            tail: None,
+            length: 0,
+            allocator: PhantomData,
         }
     }
 
-    /// Returns the number of elements in the list
-    pub fn meta(&self) -> Option<*mut B> {
-        self.meta
+    pub fn len(&self) -> usize {
+        self.length
     }
 
-    /// Returns the number of elements in the list
-    pub fn numerosity(&self) -> usize {
-        self.numerosity
-    }
-
-    /// Returns true if the list is empty
     pub fn is_empty(&self) -> bool {
-        self.numerosity == 0
+        self.length == 0
     }
 
-    /// Add an element to the front of the list
-    pub fn push_front(&mut self, value: B) -> core::result::Result<bool, A::Error> {
-        let new_node = LinkedNode::allocate_node::<A>(value)?.as_ptr()
-            as *mut LinkedNode<BytesOrigin, BytesDestination, AllocatorOrigin, B>;
-
-        // <A::Ok as crate::traits::allocatable::Pointer>::from_raw(new_node);
+    pub fn push_front(&mut self, value: T) -> bool {
+        let node = LinkedNode::allocate::<A>(value);
+        if node.is_null() {
+            return false;
+        }
 
         unsafe {
-            if let Some(old_former) = self.former {
-                (*new_node).next = Some(old_former);
-                self.former = Some(new_node);
-            } else {
-                // Empty list
-                self.former = Some(new_node);
-                self.latter = Some(new_node);
-            }
+            (*node).next = self.head;
         }
 
-        self.numerosity += 1;
-        core::result::Result::Ok(true)
+        if self.tail.is_none() {
+            self.tail = Some(node);
+        }
+
+        self.head = Some(node);
+        self.length += 1;
+        true
     }
 
-    /// Add an element to the back of the list
-    pub fn push_back(&mut self, value: B) -> core::result::Result<bool, A::Error> {
-        let new_node = LinkedNode::allocate_node::<A>(value)?.as_ptr()
-            as *mut LinkedNode<BytesOrigin, BytesDestination, AllocatorOrigin, B>;
+    pub fn push_back(&mut self, value: T) -> bool {
+        let node = LinkedNode::allocate::<A>(value);
+        if node.is_null() {
+            return false;
+        }
+
+        if let Some(tail) = self.tail {
+            unsafe {
+                (*tail).next = Some(node);
+            }
+        } else {
+            self.head = Some(node);
+        }
+
+        self.tail = Some(node);
+        self.length += 1;
+        true
+    }
+
+    pub fn pop_front(&mut self) -> Option<T> {
+        let node = self.head?;
 
         unsafe {
-            if let Some(old_latter) = self.latter {
-                (*old_latter).next = Some(new_node);
-                self.latter = Some(new_node);
-            } else {
-                // Empty list
-                self.former = Some(new_node);
-                self.latter = Some(new_node);
+            self.head = (*node).next;
+            if self.head.is_none() {
+                self.tail = None;
             }
+
+            self.length -= 1;
+            let value = core::ptr::read(core::ptr::addr_of!((*node).value));
+            let _ = LinkedNode::deallocate::<A>(node);
+            Some(value)
         }
-
-        self.numerosity += 1;
-        core::result::Result::Ok(true)
     }
 
-    /// Remove and return the element at the front of the list
-    pub fn pop_front(&mut self) -> core::result::Result<Option<B>, A::Error> {
-        let former = self.former.unwrap();
-
-        self.former = unsafe { (*former).next };
-
-        if self.former.is_none() {
-            self.latter = None;
-        }
-
-        self.numerosity -= 1;
-
-        LinkedNode::deallocate_node::<A>(former)?;
-
-        core::result::Result::Ok(None)
+    pub fn front(&self) -> Option<&T> {
+        unsafe { self.head.map(|node| (*node).value()) }
     }
 
-    /// Get a reference to the first element
-    pub fn front(&self) -> Option<&B> {
-        unsafe { self.former.map(|node| (*node).value()) }
+    pub fn back(&self) -> Option<&T> {
+        unsafe { self.tail.map(|node| (*node).value()) }
     }
 
-    /// Get a reference to the last element
-    pub fn back(&self) -> Option<&B> {
-        unsafe { self.latter.map(|node| (*node).value()) }
+    pub fn clear(&mut self) {
+        while self.pop_front().is_some() {}
     }
 
-    /// Clear all nodes and deallocate memory
-    pub fn clear(&mut self) -> core::result::Result<(), A::Error> {
-        while self.pop_front()?.is_some() {}
-        core::result::Result::Ok(())
-    }
-
-    /// Get an iterator over the values in the list
-    pub fn iter(&self) -> Iter<'_, BytesOrigin, BytesDestination, AllocatorOrigin, B> {
+    pub fn iter(&self) -> Iter<'_, T> {
         Iter {
-            current: self.former,
-            _phantom_o: PhantomData,
-            _phantom_d: PhantomData,
-            _phantom_p: PhantomData,
+            current: self.head,
+            lifetime: PhantomData,
         }
     }
 }
 
-// Implement Drop to ensure memory is freed when the list is dropped
-impl<Origin, Destination, AllocatorOrigin, B, A> Drop
-    for LinkedList<Origin, Destination, AllocatorOrigin, B, A>
+impl<T, A> Default for LinkedList<T, A>
 where
-    B: Bytes<Origin, Destination>,
-    A: Allocatable<AllocatorOrigin>,
+    A: Allocating,
+{
+    fn default() -> Self {
+        Self::new()
+    }
+}
+
+impl<T, A> Drop for LinkedList<T, A>
+where
+    A: Allocating,
 {
     fn drop(&mut self) {
-        let _ = self.clear();
+        self.clear();
     }
 }
 
-// Iterator for LinkedList
-pub struct Iter<'a, Origin: 'a, Destination: 'a, AllocatorOrigin: 'a, B: 'a>
-where
-    B: Bytes<Origin, Destination>,
-{
-    current: Option<*mut LinkedNode<Origin, Destination, AllocatorOrigin, B>>,
-    _phantom_o: PhantomData<&'a Origin>,
-    _phantom_d: PhantomData<&'a Destination>,
-    _phantom_p: PhantomData<&'a AllocatorOrigin>,
+pub struct Iter<'list, T> {
+    current: Option<*mut LinkedNode<T>>,
+    lifetime: PhantomData<&'list T>,
 }
 
-impl<'a, Origin: 'a, Destination: 'a, AllocatorOrigin: 'a, B: 'a> Iterator
-    for Iter<'a, Origin, Destination, AllocatorOrigin, B>
-where
-    B: Bytes<Origin, Destination>,
-{
-    type Item = &'a B;
+impl<'list, T> Iterator for Iter<'list, T> {
+    type Item = &'list T;
 
     fn next(&mut self) -> Option<Self::Item> {
         unsafe {
             let current = self.current?;
             let current_ref = &*current;
-            let result = current_ref.value();
             self.current = current_ref.next;
-            Some(result)
+            Some(current_ref.value())
         }
     }
 }
-
-// // Define a default constructor that uses heap allocation
-// impl<Origin, B> LinkedList<Origin, B, B>
-// where
-//     B: Bytes<Origin, Destination> + HeapAllocatable<Origin>,
-// {
-//     /// Creates a new LinkedList that uses the default heap allocation strategy
-//     pub fn with_heap_allocation() -> Self {
-//         Self::new()
-//     }
-// }
